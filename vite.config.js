83624f04502375eeafac05a74a1150c51f798757
RNKuchin <roman.kuchin@mail.com>
2025-03-28 20:05:15 +0500

    +

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/https://RNKuchin.github.io/portfolio/'
+  base: 'https://RNKuchin.github.io/portfolio'
 })
